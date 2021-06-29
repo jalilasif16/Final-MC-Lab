1c9f2fc9188e9d578baeace8682aed459f809403
Wanhar-pc <programmingwithjj@gmail.com>
2021-06-29 12:46:44 +0500

    Final Lab

---
+++ app/src/main/java/com/jalilasif/mc_jalilasif_192021/Splash.java
@@ -0,0 +1,47 @@
+package com.jalilasif.mc_jalilasif_192021;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class Splash extends AppCompatActivity {
+
+    ImageView hover;
+    TextView finallab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_splash);
+
+        hover = findViewById(R.id.hover);
+        finallab = findViewById(R.id.finallab);
+        animations();
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(Splash.this,MainActivity.class);
+            startActivity(i);
+        }, 6000);
+    }
+
+
+    public void animations(){
+
+        Animation scale_animation = AnimationUtils.loadAnimation(this,R.anim.scale_animation);
+        hover.startAnimation(scale_animation);
+
+        Animation bottom_animation = AnimationUtils.loadAnimation(this,R.anim.bottom_to_top);
+        finallab.startAnimation(bottom_animation);
+
+    }
+
+}
